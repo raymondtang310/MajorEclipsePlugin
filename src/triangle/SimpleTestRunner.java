a0dbc9dd7944773b670f107e37c013a918a8d1dc
Raymond <raymondtang310@gmail.com>
2017-03-04 21:59:59 -0500

    removed Main.java, created SimpleTestRunner.java

---
+++ src/triangle/SimpleTestRunner.java
@@ -0,0 +1,44 @@
+package triangle;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Request;
+import org.junit.runner.Result;
+
+import killmap.ExtendedTestFinder;
+import killmap.TestMethod;
+import major.mutation.Config;
+
+/**
+ * This program runs JUnit tests in TriangleTest.java.
+ * This is a simple class used for testing. 
+ * 
+ * @author Raymond Tang
+ *
+ */
+public class SimpleTestRunner {
+
+	public static void main(String[] args) {
+		Class<test.TriangleTest> c = test.TriangleTest.class;
+		Collection<TestMethod> testMethods = null;
+		try {
+			testMethods = ExtendedTestFinder.getTestMethods(c);
+		} catch (Exception e) {
+			System.out.println("tough luck kid");
+			return;
+		}
+		Config.__M_NO = 0;
+		for(TestMethod testMethod : testMethods) {
+			Result result = (new JUnitCore()).run(Request.method(testMethod.getTestClass(), testMethod.getName()));
+			System.out.println(testMethod.getName() + " " + result.wasSuccessful());
+		}
+		List<Integer> coveredMutants = Config.getCoverageList();
+		for(Integer mut : coveredMutants) {
+			System.out.println(mut);
+		}
+		Config.reset();
+	}
+
+}

867ecdd41f9b9eaffefb5f7d575b0e7d0c917673
Raymond <raymondtang310@gmail.com>
2017-04-10 23:26:45 -0400

    sorted mutant numbers in view numerically ascending

--- src/org/rayzor/mutantview/views/MutantView.java
+++ src/org/rayzor/mutantview/views/MutantView.java
@@ -84,6 +84,13 @@ public class MutantView extends ViewPart {
 		}
 	}
 	class NameSorter extends ViewerComparator {
+		@Override
+		public int compare(Viewer viewer, Object e1, Object e2) {
+			if(!(e1 instanceof Integer && e2 instanceof Integer)) return super.compare(viewer, e1, e2);
+			int e1Int = ((Integer)e1).intValue();
+			int e2Int = ((Integer)e2).intValue();
+			return e1Int - e2Int;
+		}
 	}
 
 	/**

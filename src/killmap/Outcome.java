5810b439f5b34edd655c6a053ea8b3a2a3f333ca
Raymond <raymondtang310@gmail.com>
2017-04-18 19:26:52 -0400

    added comments to Outcome.java

--- src/killmap/Outcome.java
+++ src/killmap/Outcome.java
@@ -1,5 +1,12 @@
 package killmap;
 
+/**
+ * An Outcome refers to whether a mutant is killed or not by a test. If a test kills a mutant,
+ * the Outcome is KILLED. If a test does not kill a mutant, the Outcome is ALIVE. 
+ * 
+ * @author Raymond Tang
+ *
+ */
 public enum Outcome {
 	ALIVE, KILLED
 }

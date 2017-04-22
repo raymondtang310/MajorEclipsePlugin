dc2980cd0b2f4b04b8c41dce58e0d56c22ea10dc
Raymond <raymondtang310@gmail.com>
2017-04-22 17:05:56 -0400

    updated comments in WorkOrder.java

--- src/killmap/WorkOrder.java
+++ src/killmap/WorkOrder.java
@@ -11,8 +11,9 @@ package killmap;
  *
  */
 public class WorkOrder implements Comparable<WorkOrder> {
-	
+	// The ID (number) of the mutant
 	private int mutantID;
+	// The test method
 	private TestMethod testMethod;
 	
 	public WorkOrder(int mutantID, TestMethod testMethod) {
@@ -20,14 +21,38 @@ public class WorkOrder implements Comparable<WorkOrder> {
 		this.testMethod = testMethod;
 	}
 
+	/**
+	 * Returns the mutant's ID
+	 * 
+	 * @return the mutant's ID
+	 */
 	public int getMutantID() {
 		return mutantID;
 	}
 
+	/**
+	 * Returns the test method
+	 * 
+	 * @return the test method
+	 */
 	public TestMethod getTestMethod() {
 		return testMethod;
 	}
 
+	/**
+	 * Compares the given WorkOrder to this WorkOrder. Two WorkOrders are considered
+	 * equal if their mutant IDs are equal and their test methods are equal. This WorkOrder
+	 * is considered less than the given WorkOrder if this WorkOrder's mutant ID is less than the mutant ID
+	 * of the given WorkOrder, or if their mutant IDs are the same, if this WorkOrder's test method is less than
+	 * the given WorkOrder's test method. This WorkOrder is considered greater than the given WorkOrder
+	 * if this WorkOrder's mutant ID is greater than the mutant ID of the given WorkOrder, or if their 
+	 * mutant IDs are the same, if this WorkOrder's test method is greater than the given WorkOrder's test method.
+	 * 
+	 * @param the WorkOrder to compare to this WorkOrder
+	 * @return the value 0 if these WorkOrders are the same;
+	 * 		   a value less than 0 if this WorkOrder is less than the given WorkOrder; or
+	 * 		   a value greater than 0 if this WorkOrder is greater than the given WorkOrder
+	 */
 	@Override
 	public int compareTo(WorkOrder workOrder) {
 		if(this.mutantID < workOrder.getMutantID()) return -1;
